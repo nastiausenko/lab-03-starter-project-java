2e94f267caf7675b7833e2fdcbf509fb1f788152
nastiausenko <Usenko.Anastasia@lll.kpi.ua>
2024-06-06 13:55:48 +0300

    init commit

---
+++ src/main/java/dev/usenkonastia/lab3java/Lab3JavaApplication.java
@@ -0,0 +1,12 @@
+package dev.usenkonastia.lab3java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab3JavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab3JavaApplication.class, args);
+    }
+}
